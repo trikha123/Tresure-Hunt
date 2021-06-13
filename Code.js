d29311771b7081d31eb8cfe889e53bc3cc34256c
trikha123 <76527514+trikha123@users.noreply.github.com>
2021-06-13 13:26:01 -0400

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+    fill("white")
+    textSize(15)
+    text("E T A D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores data!!", 100, 270)
+    
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,170)
+    fill("lightblue")
+    text("Hint: Performs a task!!", 700, 170)
+    
+
+}
